64d84c86053a7df4a73fd38c18dd3dff79e31325
Nata Nael <natanael280198@gmail.com>
2022-06-10 13:46:39 +0700

    First commit

---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <div class="container">
+      <h1>Welcome!</h1>
+      <a href="/restaurants">Find restaurants!</a>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.container {
+  text-align: center;
+  margin-top: 3rem;
+}
+</style>
